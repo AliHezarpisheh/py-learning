8e67ed5782abadc9c70dd26408549f96051bcf57
AliHezarpisheh <alihezarpisheh@outlook.com>
2024-02-15 20:57:04 +0330

    🐛 Fix: Eliminate malfunctioning __call__ method in LogFunctionExecutionTime class

--- tasks/clean_code/timer.py
+++ tasks/clean_code/timer.py
@@ -1,9 +1,8 @@
 """Module for measuring the execution time of a function."""
 
 import time
-import functools
 import contextlib
-from typing import Generator, Callable, Any
+from typing import Generator
 
 
 @contextlib.contextmanager
@@ -12,8 +11,8 @@ def log_function_execution_time() -> Generator[None, None, None]:
     start_time = time.time()
     yield
     end_time = time.time()
-    duration = start_time - end_time
-    print(f"Execution time: {duration} seconds")
+    duration = end_time - start_time
+    print(f"Execution time: {round(duration, 2)} seconds")
 
 
 class LogFunctionExecutionTime(contextlib.ContextDecorator):
@@ -46,39 +45,5 @@ class LogFunctionExecutionTime(contextlib.ContextDecorator):
             Variable keyword arguments.
         """
         end_time = time.time()
-        self._duration = self._start_time - end_time
-
-    def __call__(self, func: Callable) -> Any:
-        """
-        Decorates a function to measure its execution time and print relevant
-        information.
-
-        Parameters
-        ----------
-        func : Callable
-            The function to be decorated.
-
-        Returns
-        -------
-        Any
-            The decorated function.
-        """
-
-        @functools.wraps(func)
-        def wrapper(*args, **kwargs) -> None:
-            """
-            Wrapper function that measures the execution time of the decorated function
-            and prints information.
-
-            Parameters
-            ----------
-            *args : tuple
-                Variable positional arguments.
-            **kwargs : dict
-                Variable keyword arguments.
-            """
-            print(f"Function: {func.__name__}")
-            print(f"Arguments: {args}, {kwargs}")
-            print(f"Execution time: {self._duration} seconds")
-
-        return wrapper
+        duration = end_time - self._start_time
+        print(f"Execution time: {round(duration, 2)} seconds")
