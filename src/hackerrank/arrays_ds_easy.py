1d18eb968208794c5ee31d50ca6e41c39c382636
alihezarpisheh <alihezarpisheh@outlook.com>
2024-08-19 10:38:05 +0330

    ✨ Feature(hackerrank): Solve new problems

---
+++ src/hackerrank/arrays_ds_easy.py
@@ -0,0 +1,18 @@
+def reverseArray(a):
+    reversed_array = []
+
+    len_array = len(a)
+    last_elm_index = len_array - 1
+    for index in range(0, len_array):
+        elm = a[last_elm_index - index]
+        reversed_array.append(elm)
+    return reversed_array
+
+if __name__ == '__main__':
+    arr_count = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    res = reverseArray(arr)
+
+    print(res)
