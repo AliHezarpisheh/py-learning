7288ff571c86e2939d753fd128ecc543f40eb445
ali.h <ali.h@behinrahkar.com>
2025-05-27 08:20:58 +0330

    ✨ Add some metadata practices

---
+++ src/oop/meta_class_checks_and_hooks.py
@@ -0,0 +1,26 @@
+import abc
+
+
+class BirdMeta(abc.ABCMeta):
+    def __instancecheck__(self, instance: object) -> bool:
+        return hasattr(instance, "fly") and hasattr(instance, "sing")
+
+    def __subclasscheck__(self, subclass: object) -> bool:
+        return hasattr(subclass, "fly") and hasattr(subclass, "sing")
+
+
+class Bird(metaclass=BirdMeta):
+    @classmethod
+    def __subclasshook__(cls, subclass):
+        if hasattr(subclass, "fly") and hasattr(subclass, "sing"):
+            return True
+        raise NotImplemented
+
+
+class Duck:
+    def fly(self): ...
+    def sing(self): ...
+
+class Plane:
+    def fly(self): ...
+    def shoot(self): ...
