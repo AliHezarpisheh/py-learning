3d3b7019caaf8be6e98e1c8b1ac7f8523656a26a
ali.h <ali.h@behinrahkar.com>
2025-06-07 16:34:22 +0330

    ✨ Add `weakref` examples

---
+++ src/oop/weak_refrences.py
@@ -0,0 +1,40 @@
+import weakref
+
+
+class Foo: ...
+
+
+# ---- reference ----
+foo = Foo()
+weak_foo = weakref.ref(foo)
+
+del foo
+
+# ---- weak key dictionary ----
+wk = weakref.WeakKeyDictionary()
+foo = Foo()
+wk[foo] = "foo"
+
+del wk[foo]
+
+# ---- weak value dictionary ----
+wk = weakref.WeakValueDictionary()
+foo = Foo()
+wk["item"] = foo
+
+del wk["item"]
+
+# ---- weak set ----
+wk = weakref.WeakSet()
+foo1 = Foo()
+foo2 = Foo()
+foo3 = Foo()
+wk.add(foo1)
+wk.add(foo2)
+wk.add(foo3)
+
+# ---- finalize ----
+foo = Foo()
+weakref.finalize(foo, print, "Resource is deleted")
+
+del foo
